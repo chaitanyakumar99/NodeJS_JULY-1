c0a56695467ee3b6242b0f94ba5ec65d503c2590
USER 1 <gchkumar999@gmail.com>
2024-07-01 15:19:42 +0530

    txt

---
+++ Basics/OS/one.js
@@ -0,0 +1,9 @@
+import os from "os"         //ES6
+console.log(os.arch());
+console.log(os.freemem());
+console.log(os.platform());
+console.log(os.homedir());
+console.log(os.machine());
+console.log(os.tmpdir());
+console.log(os.version());
+console.log(os.type());
