823a0ba9b65508814c7268e67b16f6d34fa69baa
USER 1 <gchkumar999@gmail.com>
2024-07-03 17:17:38 +0530

    txt

--- Basics/readwritepath/pathes6.js
+++ Basics/readwritepath/pathes6.js
@@ -1,3 +1,7 @@
 import path from 'path'
 
-console.log(path.join(__dirname));
+// console.log(path.join(__dirname));
+// console.log(path.join(__filename));
+
+console.log(path.join(process.cwd()));
+console.log(path.join(process.cwd(),"india","bangalore"));
