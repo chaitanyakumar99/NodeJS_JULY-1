adb9f3d5a020aacad3c1b05d27bf198dae3fb541
USER 1 <gchkumar999@gmail.com>
2024-07-03 17:24:41 +0530

    txt

--- Basics/readwritepath_ES6/pathes6.js
+++
@@ -1,5 +0,0 @@
-import fs from 'fs'
-
-import path  from 'path'
-
-fs.readFile.path(path.join())
--- Basics/readwritepath_ES6/read.js
+++
@@ -1,4 +0,0 @@
-import fs from 'fs'
-
-import path from 'path'
-let file_Name=path.join(process.cwd(),"in","bn")
