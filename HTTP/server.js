364f18ec2ebcf2109db8ed7b0b21f56ba012e70c
USER 1 <gchkumar999@gmail.com>
2024-07-04 16:33:21 +0530

    txt

--- HTTP/server.js
+++ HTTP/server.js
@@ -1,7 +1,7 @@
 const http=require('http')
 
 let server =http.createServer((req,resp)=>{
-    resp.end("<h1>Hello GM</h1>")
+    resp.end("<h1>Hello GM....!</h1>")
 })
 
 server.listen(8080,(err)=>{
