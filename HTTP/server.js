093bf061d3f22027a3e8d3392fcf95ca24641f0d
USER 1 <gchkumar999@gmail.com>
2024-07-04 11:24:44 +0530

    txt

---
+++ HTTP/server.js
@@ -0,0 +1,10 @@
+const http=require('http')
+
+let server =http.createServer((req,resp)=>{
+    resp.end("<h1>Hello GM</h1>")
+})
+
+server.listen(8080,(err)=>{
+    if(err) throw err
+    console.log("Server is Running");
+})
