1a210aaa0e226de53f9f96130aae946bdb149de9
USER 1 <gchkumar999@gmail.com>
2024-07-05 11:19:04 +0530

    txt

--- HTTP_EX2/server.js
+++ HTTP_EX2/server.js
@@ -4,7 +4,7 @@ let server =http.createServer((req,resp)=>{
     resp.end("<h1>Hi, Hello....!</h1>")
 })
 
-server.listen(1111,(err)=>{
+server.listen(8080,"127.0.0.1",(err)=>{
     if(err) throw err
     console.log("Server is Running")
 })
