7db894b97844d33d166003767f071f598ede1013
USER 1 <gchkumar999@gmail.com>
2024-07-09 23:16:11 +0530

    txt

--- ThirdParty/server.js
+++ ThirdParty/server.js
@@ -3,3 +3,15 @@ const http=require('http')
 const dotenv=require('dotenv')
 
 dotenv.config({path:'./config.env'})
+
+let port=process.env.port
+let host_Name=process.env.host_Name
+
+let server=http.createServer((req,resp)=>{
+    resp.end("Http server.....")
+})
+
+Server.listen(port,host_Name,(err)=>{
+    if(err) throw err;
+    console.log(`server Running on htttp://${host_Name}:${port}/`);
+})
