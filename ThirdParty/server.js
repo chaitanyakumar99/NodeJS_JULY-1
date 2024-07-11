a8c758af3d6cc48dcb3684b38cfca4539e358460
USER 1 <gchkumar999@gmail.com>
2024-07-11 11:46:35 +0530

    txt

--- ThirdParty/server.js
+++ ThirdParty/server.js
@@ -1,17 +1,32 @@
-const http=require('http')
+// const http=require('http')
 
-const dotenv=require('dotenv')
+// const dotenv=require('dotenv')
 
-dotenv.config({path:'./config.env'})
+// dotenv.config({path:'./config/dev.env'})
 
-let port=process.env.PORT
-let host_Name=process.env.HOST_Name
+// let port=process.env.PORT
+// let host_Name=process.env.HOST_NAME
 
-let server=http.createServer((req,resp)=>{
-    resp.end("Http server.....")
+// let server=http.createServer((req,resp)=>{
+//     resp.end("Http server.....")
+// })
+
+// server.listen(port,host_Name,(err)=>{
+//     if(err) throw err;
+//     console.log(`server Running on http://${host_Name}:${port}/`);
+// })
+
+const http = require ('http')
+const dotenv= require('dotenv')
+dotenv.config({path:'./config/dev.env'})
+let port = process.env.PORT
+let hostname=process.env.HOST_NAME
+// console.log(port);
+// console.log(hostname);
+let server= http.createServer((req,resp)=>{
+    resp.end("Http server.........!!");
 })
-
-Server.listen(port,host_Name,(err)=>{
-    if(err) throw err;
-    console.log(`server Running on htttp://${host_Name}:${port}/`);
+server.listen(port,hostname,(err)=>{
+    if(err) throw err
+    console.log(`serveris running http://${hostname}:${port}/`)
 })
