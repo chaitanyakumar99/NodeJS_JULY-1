1b2b71e187e07d8f3b37556c0e5c4213d825068a
USER 1 <gchkumar999@gmail.com>
2024-07-15 17:43:01 +0530

    txt

--- Bcryptjwt_ex/app1.js
+++ Bcryptjwt_ex/app1.js
@@ -10,6 +10,17 @@
  let one=bcrypt.genSaltSync(10)
  let new_name=bcrypt.hashSync(user.name,one)
  console.log(new_name);
+ user={...user,name:new_name}
+ console.log(user)
  
- let name=bcrypt.compareSync("Rahul",user.name)
- console.log(name);
+ let dd=bcrypt.compareSync("Rahul",user.name)
+ if(dd){
+   console.log("login successfully")
+ }   
+ else{
+   console.log("login failed")
+ }   
+
+
+
+ 
