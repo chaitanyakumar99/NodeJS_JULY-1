95ec57cbca31e2d80b5d4959b9b1c1bcc5283101
USER 1 <gchkumar999@gmail.com>
2024-07-17 10:39:57 +0530

    txt

--- CRUD_json/post-method_create/app1.js
+++ CRUD_json/post-method_create/app1.js
@@ -4,19 +4,23 @@ import router from './routing/router.js'
 
 let app=express()
 
-app.use(express.json());
+ app.use(express.json());
 
 dotenv.config({path:"./config/dev.env"})
 
 let port=process.env.port
 let host=process.env.hostname
 
+
 app.get("/",(req,resp)=>{
     resp.send("server running")
 })
 
-app.use("/emp",router)
+
+
+app.use("/hello",router)
 app.listen(port,host,(err)=>{
     if(err) throw err
     console.log(`server Running http://${host}:${port}/`);
 })
+
--- CRUD_json/post-method_create/routing/router.js
+++ CRUD_json/post-method_create/routing/router.js
@@ -1,29 +1,38 @@
 import express from 'express'
 import fs from 'fs'
 
-let router=express.Router()
+let router = express.Router()
 
 
-router.post("/create",async(req,resp)=>{
-let emp_data=req.body
- let emp=await hi()
-  let flag=emp.find((abc)=>{
- return abc.id===emp_data.id
+router.post("/create", async (req, resp) => {
+  let emp_data = req.body
+  console.log(emp_data);
+  let emp = await hi()
+  let flag = emp.find((abc) => {
+    return abc.id === emp_data.id
   })
 
-  if(flag){
-    return resp.json({"err":" user already exist"})
+  if (flag) {
+    return resp.json({ "err": " user already exist" })
   }
   emp.push(emp_data)
   await saveEmployees(emp)
-  return resp.status(200).json({"msg":"new employeee created"})
+  return resp.status(200).json({ "msg": "new employeee created" })
+})
+
+router.get("/read",async (req,resp)=>{
+  let emp=await hi()
+   resp.status(200).json(emp)
+})
+
+let saveEmployees=((emp)=>{
+  fs.writeFileSync('data.json',JSON.stringify(emp))
 })
 
 
 
-
-let hi=()=>{
-    let employees=fs.readFileSync('data.json','utf-8')
-    return JSON.parse(employees)
+let hi = () => {
+  let employees = fs.readFileSync('data.json', 'utf-8')
+  return JSON.parse(employees)
 }
 export default router
