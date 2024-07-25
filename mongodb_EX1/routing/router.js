97181833d4bbb672a009a2dfe54e007994fdfc15
USER 1 <gchkumar999@gmail.com>
2024-07-25 21:51:14 +0530

    txt

--- mongodb_EX1/routing/router.js
+++ mongodb_EX1/routing/router.js
@@ -44,14 +44,23 @@ router.post('/create', async (req, resp) => {
 //usage:update employee_data
 //Method:PUT
 
-router.put('/update/:id',async (req, resp) => {
-try{
-let emp=req.params.eid
-
-}
-catch(err){
-
-}
+router.put('/update/:id', async (req, resp) => {
+    try {
+        let emp = req.body
+        console.log(emp);
+        let emp_Id = req.params.eid
+        console.log(emp_Id);
+        let employee=await model.findOne({"eid":emp_Id})
+        console.log(employee);
+        if(!employee){
+         return resp.status(200).json({"msg":"Employee not Exits"})
+        }
+        await model.findByIdAndUpdate(employee._eid,{$set:{ename:emp.ename,eal:emp.esal}});
+        return resp.status(200).json(({"msg":"Employee updated sucessfully"}))
+    }
+    catch (err) {
+        return resp.status(200).json({"err":err.message})
+    }
 })
 
 
@@ -59,7 +68,22 @@ catch(err){
 //Method:DELETE
 
 router.delete('/delete/:id', (req, resp) => {
-
+try{
+    try{
+        let emp_Id = req.params.id;
+        console.log(emp_Id)
+        let Employee  =await EmployeeModel.findOne({eid:emp_Id})
+        console.log(Employee)
+        if(!Employee){
+          return resp.status(401).json({"msg":"Employee Not Exists"})
+        }
+        await EmployeeModel.findByIdAndDelete(Employee._id);
+        return resp.status(200).json({"msg":"Employee Document Deleted Sucessfully"})
+      }
+}
+catch(err){
+    
+}
 })
 
 export default router
