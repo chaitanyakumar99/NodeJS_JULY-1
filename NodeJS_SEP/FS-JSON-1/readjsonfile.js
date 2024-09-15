8e961880828a098aabdb21b4cafc15116c2697ef
chaitanyakumar99 <gchkumar999@gmail.com>
2024-09-15 19:46:22 +0530

    txt

---
+++ NodeJS_SEP/FS-JSON-1/readjsonfile.js
@@ -0,0 +1,11 @@
+import fs from 'fs'
+
+fs.readFile('data.json','utf-8',(err,data)=>{
+if (err)throw err
+console.log(data)
+
+fs.writeFile('emp.json',data,(err)=>{
+    if(err)throw err
+    console.log("New Json file created..!")
+})
+})
