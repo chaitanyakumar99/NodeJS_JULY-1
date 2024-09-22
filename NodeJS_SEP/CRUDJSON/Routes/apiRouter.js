41d47ea403a797633154360303990beec5fe17f5
chaitanyakumar99 <gchkumar999@gmail.com>
2024-09-22 14:31:17 +0530

    txt

--- NodeJS_SEP/CRUDJSON/Routes/apiRouter.js
+++ NodeJS_SEP/CRUDJSON/Routes/apiRouter.js
@@ -1,28 +1,35 @@
 import express from 'express'
 import fs from 'fs'
-import { Router } from 'express'
+let router =express.Router();
 
-let router=express.Router()
+// Usage : create new employee/user/product/order
+// API URL: http://127.0.0.1:8080/api/create
+// Method Type: POST
+// Required Fields: eid,ename,esal,loc
+// Access Type:Public
+// Note: we need to verify employee exist or not. 
+// If employee not exit we are going to create new employee
 
-/*
-usage:create 
-Api url:
-Method type:POST
-Required Field:eid,ename,esal,eloc
-Acess type:public             
-*/
+router.post("/create",(req,resp)=>{
+    let emp =req.body;
+});
 
-router.post("/create",(Req,resp)=>{
-    let emp =req.body
+
+// Usage : fetch all employees/users/products/orders
+// API URL: http://127.0.0.1:8080/api/read
+// Method Type: GET
+// Required Fields: None
+// Access Type:Public
+
+router.get("/read",async(req,resp)=>{
+    let employees= await getEmployees()
+    return resp.json(employees)
 })
-
-router.post("/create",(re,resp)=>{})
-router.get("/read",(re,resp)=>{})
-router.put("/update:id",(re,resp)=>{})
-router.delete("/delete:id",(re,resp)=>{})
 
 
 let getEmployees=()=>{
-    
+    let emp_Data=fs.readFileSync('data.json','utf-8')
+    return JSON.parse(emp_Data)
 }
-export default Router
+
+export default router;
--- NodeJS_SEP/CRUDJSON/app.js
+++ NodeJS_SEP/CRUDJSON/app.js
@@ -1,19 +1,30 @@
 import express from 'express'
 import morgan from 'morgan'
 import dotenv from 'dotenv'
+import apiRouter from './Routes/apiRouter.js'
+
+//create express app
 
 let app=express()
 
+//load confirgation values
 
-dotenv.config({path:'./confog/product.config'})
+dotenv.config({path:'./config/product.config'})
 
+//http longer
 app.use(morgan('tiny'))
 
 let port=process.env.port
 let host=process.env.host
 
+
 app.get("/",(req,resp)=>{
-    resp.json("Root req")
+    resp.json({"msg":"RootRequest"})
 })
 
+app.use("/api",apiRouter);
 
+app.listen(port,host,(err)=>{
+    if(err)throw err
+    console.log(`server is Running http://${host}:${port}/`)
+})
