4b1b0462398e5212f181cbfc6256a0f15b1910fb
chaitanyakumar99 <gchkumar999@gmail.com>
2024-10-10 18:00:09 +0530

    txt

--- NodeJS_SEP/ExpressEX-All/expressRouter/app.js
+++ NodeJS_SEP/ExpressEX-All/expressRouter/app.js
@@ -6,12 +6,13 @@ let app = express()
 app.use(morgan('tiny'));
 
 //API URL: 127.0.0.1:8080/
+
 app.get("/", (req, resp) => {
-    resp.send("Root Request")            
+    resp.send("Root Request")
 })
 app.use("/user", userRouter)
 
 app.listen(8080, '127.0.0.1', (err) => {
     if (err) throw err
-    console.log(`Server is Running on .. http://127.0.0.1:8080/`)
+    console.log(`Server is Running on... http://127.0.0.1:8080/`)
 })
