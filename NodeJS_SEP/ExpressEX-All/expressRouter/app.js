9ef58a3b697468849563ba2f1a925cf95919a20f
chaitanyakumar99 <gchkumar999@gmail.com>
2024-10-10 17:53:05 +0530

    txt

--- NodeJS_SEP/ExpressEX-All/expressRouter/app.js
+++ NodeJS_SEP/ExpressEX-All/expressRouter/app.js
@@ -1,13 +1,13 @@
 import express from 'express'
 import morgan from "morgan"
-import userRouter from './routing/userRouter.js'
+import userRouter from './routing/userRouter'
 let app = express()
 
 app.use(morgan('tiny'));
 
 //API URL: 127.0.0.1:8080/
 app.get("/", (req, resp) => {
-    resp.send("Root Request")
+    resp.send("Root Request")            
 })
 app.use("/user", userRouter)
 
