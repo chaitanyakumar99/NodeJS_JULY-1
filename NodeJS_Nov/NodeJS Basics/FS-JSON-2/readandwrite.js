66f92e23642eac952f6cbd4f198b6a2f21f966bb
chaitanyakumar99 <gchkumar999@gmail.com>
2024-12-01 23:33:06 +0530

    txt

---
+++ NodeJS_Nov/NodeJS Basics/FS-JSON-2/readandwrite.js
@@ -0,0 +1,29 @@
+import fs from 'fs'
+import { type } from 'os'
+
+fs.readFile("data.json",'utf-8',(err,data)=>{
+    if(err) throw err 
+    let users = JSON.parse(data);
+
+
+    let male_Users=[]
+    let female_Users=[]
+
+    for(let user of users){
+            if(user.gender ==="Male"){
+                male_Users.push(user)
+            }
+            if(user.gender ==="Female"){
+                female_Users.push(user)
+            }
+    }
+    fs.writeFile("male.json", JSON.stringify(male_Users),(err)=>{
+        if(err) throw err 
+        console.log("New JSON File Created") 
+    })
+    fs.writeFile("fmale.json", JSON.stringify(female_Users),(err)=>{
+        if(err) throw err 
+        console.log("New JSON File Created") 
+    })
+
+})
